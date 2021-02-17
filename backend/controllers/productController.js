2711fe50686017aeff07660c9606c50b71eaaf7d
Hafriz Jafni <hafriz.jafni@gmail.com>
2021-02-17 10:52:20 +0700

    Fetch a single and all products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,8 @@
 import Product from '../models/productModel.js';
 
+// @desc    Create new product
+// @route   POST /api/v1/product/new
+// @access  Private/Admin
 const newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
 
@@ -9,11 +12,36 @@ const newProduct = async (req, res, next) => {
   });
 };
 
-const getProducts = (req, res) => {
+// @desc    Get all products
+// @route   GET /api/v1/products
+// @access  Public
+const getProducts = async (req, res) => {
+  const products = await Product.find();
+
   res.status(200).json({
     succes: true,
-    message: 'This route will show all products in database.',
+    count: products.length,
+    products,
   });
 };
 
-export { newProduct, getProducts };
+// @desc    Get a single product
+// @route   GET /api/v1/product/:id
+// @access  Public
+const getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      message: 'Product not found.',
+    });
+  }
+};
+
+export { newProduct, getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
-import { newProduct, getProducts } from '../controllers/productController.js';
+import {
+  newProduct,
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getProductById);
 router.route('/product/new').post(newProduct);
 
 export default router;
